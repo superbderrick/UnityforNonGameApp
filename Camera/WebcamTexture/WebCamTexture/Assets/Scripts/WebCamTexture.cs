f4b341fec920e75718b1bdc70333651beec491ee
derrick_kang <derrick.kang@unity3d.com>
2019-08-01 16:16:57 +0900

    working on webcamtexture sample

---
+++ Camera/WebcamTexture/WebCamTexture/Assets/Scripts/WebCamTexture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamTexture : MonoBehaviour
+{
+    public int Width = 1920;
+    public int Height = 1080;
+    public int FPS = 30;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupForWebCamTexture();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetupForWebCamTexture()
+    {
+        var devices = WebCamTexture.devices;
+        if (devices.Length > 0)
+        {
+            WebCamTexture webcamTexture = new WebCamTexture(Width, Height, FPS);
+            gameObject.GetComponent<Renderer>().material.mainTexture = webcamTexture;
+            webcamTexture.Play();
+        }else
+        {
+            Debug.Log("Webカメラが検出できませんでした");
+            return;
+        }
+    }
+}
