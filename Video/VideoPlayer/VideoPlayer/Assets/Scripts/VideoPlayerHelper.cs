be422af699b6dd0d62a2afa547051194bf75e26a
superbderrick <kang.derrick@gmaill.com>
2019-08-05 10:10:23 +0900

    working on videoPlayer sample

---
+++ Video/VideoPlayer/VideoPlayer/Assets/Scripts/VideoPlayerHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoPlayerHelper : MonoBehaviour
+{
+    public RawImage videoScreen;
+    public VideoClip videoClip;
+    
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+
+    private void Awake()
+    {
+        SetupVideoPlayer();
+    }
+
+    private void OnDestroy()
+    {
+        videoPlayer.prepareCompleted -= OnPrepareCompleted;
+    }
+
+
+    private void SetupVideoPlayer()
+    {
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        videoPlayer.playOnAwake = false; 
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = videoClip;
+        
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted  += OnPrepareCompleted;
+    }
+
+    private void OnPrepareCompleted(VideoPlayer source)
+    {
+        videoScreen.texture = videoPlayer.texture;
+        videoPlayer.Play();
+
+    }
+    
+}
